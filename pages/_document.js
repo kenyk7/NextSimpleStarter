53eb4cd98a93777e904dd82af1ff98dd512371a4
Kenyk7 <kenykalin@gmail.com>
2017-12-13 14:44:15 -0500

    Update favicons and obs pwa lighthouse

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,15 @@ import stylesheet from '../scss/index.scss';
 export default class MyDocument extends Document {
 	render() {
 		return (
-			<html>
+			<html lang="es">
 				<Head>
 					<base href="/" />
 					<meta charSet="utf-8"/>
-					<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,minimal-ui"/>
-    			<link href="static/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+					<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=2,minimal-ui"/>
+    			<link href="static/favicons/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+    			<link rel="apple-touch-icon" sizes="180x180" href="static/favicons/apple-touch-icon.png"/>
+					<link rel="icon" type="image/png" sizes="32x32" href="static/favicons/favicon-32x32.png"/>
+					<link rel="icon" type="image/png" sizes="16x16" href="static/favicons/favicon-16x16.png"/>
 					<meta name="theme-color" content="#000000"/>
 					<link rel="manifest" href="static/manifest.json"/>
 					<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.deep_purple-blue.min.css"/>
